5906d43d2978d2c54619154b5d9d8eb4301a8af6
KidKat <gunawan.kuncoro@binus.ac.id>
2026-03-28 23:12:25 +0700

    Updates

--- Headphones.jsx
+++ Headphones.jsx
@@ -4,11 +4,12 @@ Command: npx gltfjsx@6.2.16 headphones.glb --transform
 Files: headphones.glb [675.32KB] > D:\Web Development\Projects\3D Showcase Web\public\headphones-transformed.glb [638.6KB] (5%)
 */
 
-import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
 export function Model(props) {
-  const { nodes, materials } = useGLTF("/headphones-transformed.glb");
+  const { nodes, materials } = useGLTF(
+    `${import.meta.env.BASE_URL}headphones-transformed.glb`,
+  );
   return (
     <group {...props} dispose={null}>
       <mesh
@@ -57,4 +58,4 @@ export function Model(props) {
   );
 }
 
-useGLTF.preload("/headphones-transformed.glb");
+useGLTF.preload(`${import.meta.env.BASE_URL}headphones-transformed.glb`);
--- Laptop.jsx
+++ Laptop.jsx
@@ -4,22 +4,46 @@ Command: npx gltfjsx@6.2.16 laptop.glb --transform
 Files: laptop.glb [1.87MB] > D:\GLTF\laptop-transformed.glb [128.58KB] (93%)
 */
 
-import React, { useRef } from 'react'
-import { useGLTF } from '@react-three/drei'
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
 
 export function Model(props) {
-  const { nodes, materials } = useGLTF('/laptop-transformed.glb')
+  const { nodes, materials } = useGLTF(
+    `${import.meta.env.BASE_URL}laptop-transformed.glb`,
+  );
   return (
     <group {...props} dispose={null}>
-      <mesh geometry={nodes.screen.geometry} material={materials.screen} position={[0, -0.09, 0.048]} rotation={[1.942, 0.011, 2.224]} scale={0.154} />
-      <mesh geometry={nodes.body.geometry} material={materials.body} position={[0, -0.09, 0.048]} rotation={[1.942, 0.011, 2.224]} scale={0.154} />
-      <mesh geometry={nodes.webcam.geometry} material={materials.webcam} position={[0, -0.09, 0.048]} rotation={[1.942, 0.011, 2.224]} scale={0.154} />
-      <group position={[0, -0.09, 0.048]} rotation={[1.942, 0.011, 2.224]} scale={0.154}>
+      <mesh
+        geometry={nodes.screen.geometry}
+        material={materials.screen}
+        position={[0, -0.09, 0.048]}
+        rotation={[1.942, 0.011, 2.224]}
+        scale={0.154}
+      />
+      <mesh
+        geometry={nodes.body.geometry}
+        material={materials.body}
+        position={[0, -0.09, 0.048]}
+        rotation={[1.942, 0.011, 2.224]}
+        scale={0.154}
+      />
+      <mesh
+        geometry={nodes.webcam.geometry}
+        material={materials.webcam}
+        position={[0, -0.09, 0.048]}
+        rotation={[1.942, 0.011, 2.224]}
+        scale={0.154}
+      />
+      <group
+        position={[0, -0.09, 0.048]}
+        rotation={[1.942, 0.011, 2.224]}
+        scale={0.154}
+      >
         <mesh geometry={nodes.kayboard.geometry} material={materials.keycaps} />
         <mesh geometry={nodes.kayboard_1.geometry} material={materials.glow} />
       </group>
     </group>
-  )
+  );
 }
 
-useGLTF.preload('/laptop-transformed.glb')
+useGLTF.preload(`${import.meta.env.BASE_URL}laptop-transformed.glb`);
